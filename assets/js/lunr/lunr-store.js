b48c9d7aa4d9fc88cf8832c49bd04b301a0ff140
iamaditia <iamaditia@users.noreply.github.com>
2021-02-21 00:46:07 +0000

    jekyll build from Action b5df92ca9d6f8e87c625c0816b184d304d6c0431

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First Post",
+        "excerpt":"This is just a test post.   This is heading 1   And this is heading 2   Can you see this?   install.packages(\"ggplot2\")   Anyway   Can I test this plugin?   Now I’m trying to create a tooltip with HTML.   ","categories": [],
+        "tags": [],
+        "url": "/first-post/",
+        "teaser": null
+      }]
